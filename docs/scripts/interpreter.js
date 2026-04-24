ff092fbc9cee6e1b9f8001c14a3dcac2e1f79fb3
Noniks <pandochk1babuk9@gmail.com>
2026-04-24 13:21:30 +0300

    Update interpreter.js

--- docs/scripts/interpreter.js
+++ docs/scripts/interpreter.js
@@ -1,1 +1,19 @@
+const consoleForm = document.getElementById('consoleForm');
 
+const syntaxJSON = './json/CSyntax.json';
+
+function loadSolution(solutions) {
+
+}
+
+function interprete(tokens) {
+
+}
+
+function tokenize(code) {
+    
+}
+
+consoleForm.addEventListener('submit', (c) => {
+    tokenize(c);
+});
